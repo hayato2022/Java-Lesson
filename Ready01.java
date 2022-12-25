29b029b2fe081646aca9a5a008aa8ce02af821c8
hayato2022 <gaomusunxiang@gmail.com>
2022-12-25 13:35:26 +0000

    Java基本文法

--- Ready01.java
+++ Ready01.java
@@ -1,5 +1,9 @@
+// ↓クラスブロックの始まり
 public class Ready01 {
+    // ↓メソッドブロックの始まり(インデントをしてわかりやすくする)
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // ↓文字列を出力する処理
+        System.out.println("Hello  WEBCAMP!");
+        System.out.println("Hello Java!");
     }
 }
