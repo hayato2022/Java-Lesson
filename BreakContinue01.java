55eb170de4695138813db426667c350e968e520e
hayato2022 <gaomusunxiang@gmail.com>
2023-01-11 07:22:17 +0000

    continue文

--- BreakContinue01.java
+++ BreakContinue01.java
@@ -9,5 +9,14 @@ public class BreakContinue01 {
             }
             System.out.println("Break01= " + count);
         }
+
+        // continue文とは、ループ処理の途中でその後の処理をスキップして、ループ処理の最初から実行させたい場合に使用
+        for (int count = 0; count < 5; count++) {
+            if (count == 2) {
+                // countが2の場合、この後の処理をスキップ
+                continue;
+            }
+            System.out.println("Continue01= " + count);
+        }
     }
 }
