08d46c845c7d21564201eecc6cdcaedb73b1d278
hayato2022 <gaomusunxiang@gmail.com>
2023-01-11 02:53:09 +0000

    拡張for文

--- For01.java
+++ For01.java
@@ -7,5 +7,13 @@ public class For01 {
         for (int number = 1; number <= 5; number++) {
             System.out.println("For01= " + number);
         }
+
+        // 拡張for文：
+        // 配列の要素を順番にnumberに代入し、要素分繰り返す
+        int[] array = {1,2,3,4,5};
+        for (int number : array){
+            // ブロック：numberを出力
+            System.out.println("For02= " + number);
+        }
     }
 }
