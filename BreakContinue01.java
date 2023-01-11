1504f8c879ca43c01a2f54bacb26d2cceac10afe
hayato2022 <gaomusunxiang@gmail.com>
2023-01-11 03:20:03 +0000

    for文-break文

---
+++ BreakContinue01.java
@@ -0,0 +1,13 @@
+public class BreakContinue01 {
+    public static void main(String[] args) {
+
+      // break文とは、switch文のbreak文と同じで、ループ処理の途中でループ処理を終了させたい場合に使用する制御文
+        for (int count = 0; count < 5; count++) {
+            if (count == 2) {
+                // countが2の場合、for文を終了
+                break;
+            }
+            System.out.println("Break01= " + count);
+        }
+    }
+}
