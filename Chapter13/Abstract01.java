8231e7f7a0ed90a32ef95ee4d2bbcddc88467c29
hayato2022 <gaomusunxiang@gmail.com>
2023-01-24 02:53:50 +0000

    [継承] 抽象クラス

--- Chapter13/Abstract01.java
+++ Chapter13/Abstract01.java
@@ -3,10 +3,15 @@ public class Abstract01 {
         // Dogクラスのインスタンスを生成
         Dog dog = new Dog();
 
+        Cat cat = new Cat();
+
         // 抽象クラス（Animalクラス）のメソッドを呼び出し
         dog.eat();
         // 抽象メソッドを実装したメソッドを呼び出し
         dog.cry();
+        
+        cat.eat();
+        cat.cry();
 
          // 抽象クラスはインスタンス生成するとコンパイルエラー
         // Animal animal = new Animal();
---
+++ Chapter13/Cat.java
@@ -0,0 +1,9 @@
+public class Cat extends Animal {
+    public Cat() {
+        this.name = "猫";
+    }
+
+    public void cry() {
+        System.out.println(this.name + "が鳴きました。ニャーニャー");
+    }
+}
